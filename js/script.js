dcc4c016ebafb0c86a69be9384650d7a6a779fa9
Sam Glerum <samuelglerum@hotmail.com>
2017-10-15 01:30:52 +0200

    removed unnecesary javascript

--- js/script.js
+++ js/script.js
@@ -117,17 +117,3 @@ particlesJS("particle-js", {
     article.setAttribute("id", button_id);
     button.parentNode.replaceChild(article, button);
   }
-
-//   function LoadFile() {
-//     var oFrame = document.getElementById("frmFile");
-//     var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
-//     while (strRawContents.indexOf("\r") >= 0)
-//         strRawContents = strRawContents.replace("\r", "");
-//     var arrLines = strRawContents.split("\n");
-//     alert(strRawContents);
-//     alert("File " + oFrame.src + " has " + arrLines.length + " lines");
-//     for (var i = 0; i < arrLines.length; i++) {
-//         var curLine = arrLines[i];
-//         alert(curLine);
-//     }
-// }
